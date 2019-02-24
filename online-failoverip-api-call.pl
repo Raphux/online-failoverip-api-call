67e4416831538c58588f8a08c0d9ff6c90235a25
root <root@crm1.biocoiff.com>
2019-02-24 21:36:43 +0100

    Enhancements

--- online-failoverip-api-call.pl
+++ online-failoverip-api-call.pl
@@ -25,17 +25,18 @@ my $apitoken = "";
 my $failoverip = "";
 my $destinationip = "";
 my $online_api_url = "https://api.online.net/api/v1/server/failover";
-#my $online_api_url = "https://www.raphux.com/api/v1/server/failover";
-
+my ($silence, $negate);
 GetOptions (
-	"action=s" 		=> \$action,		"a=s" => \$action,
-	"apitoken=s"	 	=> \$apitoken,		"t=s" => \$apitoken,
-	"failoverip=s" 		=> \$failoverip,	"f=s" => \$failoverip,
-	"destinationip=s"	=> \$destinationip,	"d=s" => \$destinationip
+	"action=s" 		=> \$action,		"a=s"	=> \$action,
+	"apitoken=s"	 	=> \$apitoken,		"t=s"	=> \$apitoken,
+	"failoverip=s" 		=> \$failoverip,	"f=s"	=> \$failoverip,
+	"destinationip=s"	=> \$destinationip,	"d=s"	=> \$destinationip,
+	"negate"		=> \$negate,		"n"	=> \$negate,
+	"silence"		=> \$silence,		"s"	=> \$silence
 );
 
 sub print_usage(){
-	print("Usage : $0 --action=ACTION --apitoken=API_TOKEN --failoverip=FAILOVER_IP --destinationip=DESTINATION_IP\n");
+	print("Usage : $0 --action|-a=ACTION --apitoken|-t=API_TOKEN --failoverip|-f=FAILOVER_IP --destinationip|-d=DESTINATION_IP [--negate|-n] [--silence|-s]\n");
 	print("Where :\n");
 	print("\tACTION is one of the following choice :\n");
 	print("\t\t- start : migrate the FAILOVER_IP to DESTINATION_IP target\n");
@@ -44,6 +45,8 @@ sub print_usage(){
 	print("\tAPI_TOKEN is the TOKEN given in online.net console (https://console.online.net/en/api/access)\n");
 	print("\tFAILOVER_IP is the failover IP\n");
 	print("\tDESTINATION_IP is the destination IP on which to point the failover IP\n");
+	print("\t[--negate|-n] : when we just want to check that the failover IP is NOT assign to the destination. (optional)\n");
+	print("\t[--silence|-s] : do all the stuff, silently. (optional)\n");
 }
 
 
@@ -131,37 +134,39 @@ if ( "$action" eq "status" ){
 	$client->addHeader("X-Pretty-JSON", "0");
 
 	my $url = "$online_api_url";
+	my $json_response;
 
 	$client->GET($url);
-	my $json_response = decode_json($client->responseContent());
+	if ( $client->responseCode() == 200 ){
+		$json_response = decode_json($client->responseContent());
+	} else {
+		print "Failed to get failover info " . $client->responseCode();
+		exit 1;
+	}
 	foreach my $ip_failover (@$json_response) {
 		if ( $ip_failover->{'source'} eq "$failoverip" ) {
 			if (( ! $ip_failover->{'destination'}) and (! $destinationip)){
-				print "Info : Failover IP is not assigned to any destination.\n";
+				print "Info : Failover IP is not assigned to any destination.\n" unless ($silence);
 				exit 0;		
-			} elsif (( ! $destinationip ) and ( $ip_failover->{'destination'})){
-				print 'Info : Failover IP is assigned to ' . $ip_failover->{'destination'} . " .\n";
+			} elsif (( ! $destinationip ) and ( $ip_failover->{'destination'}) and ( ! $negate )){
+				print 'Info : Failover IP is assigned to ' . $ip_failover->{'destination'} . " .\n" unless ($silence);
 				exit 0;		
+			} elsif (( $destinationip ) and ( $ip_failover->{'destination'}) and ( $negate )){
+				print 'CRITICAL : Failover IP is assigned to ' . $ip_failover->{'destination'} . ", whereas it should NOT !\n" unless ($silence);
+				exit 1;		
 			} elsif (( $destinationip ) and ( ! $ip_failover->{'destination'} )){
-				print "Critical : Failover IP is NOT assigned to $destinationip !\n";
+				print "CRITICAL : Failover IP is NOT assigned to $destinationip !\n" unless ($silence);
 				exit 1;
 			} elsif (( $destinationip ) and ( $ip_failover->{'destination'} eq "$destinationip" )) {
-				print "OK : Failover IP is correctly assigned to $destinationip.\n";
+				print "OK : Failover IP is correctly assigned to $destinationip.\n" unless ($silence);
 				exit 0;
-			} elsif (( $destinationip ) and ( $ip_failover->{'destination'} ne "$destinationip" )){
-				print "Critical : Failover IP is NOT assigned to $destinationip !\n";
+			} elsif (( $destinationip ) and ( $ip_failover->{'destination'} ne "$destinationip" ) and ( ! $negate )){
+				print "CRITICAL : Failover IP is NOT assigned to $destinationip !\n" unless ($silence);
 				exit 1;
+			} elsif (( $destinationip ) and ( $ip_failover->{'destination'} ne "$destinationip" ) and ( $negate )){
+				print "Info : Failover IP is not assigned to $destinationip !\n" unless ($silence) ;
+				exit 0;
 			}
 		}
 	}
 }
-	
-
-
-
-
-
-
-
-
-
